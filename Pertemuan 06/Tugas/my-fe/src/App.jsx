122121c0167eceaa5e5077bf4af7e8c77321a882
Mochammad Fauzi Nur Fadilah <nurfadilahmfauzi@gmail.com>
2026-04-26 06:13:20 +0700

    push tugas pertemuan 06

---
+++ Pertemuan 06/Tugas/my-fe/src/App.jsx
@@ -0,0 +1,10 @@
+import MahasiswaPages from "./pages/MahasiswaPages";
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <MahasiswaPages />
+    </div>
+  )
+}

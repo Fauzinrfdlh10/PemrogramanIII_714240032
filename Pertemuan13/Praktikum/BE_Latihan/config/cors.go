0881982566efd6d3ed3ef988a78376215abda0e9
Mochammad Fauzi Nur Fadilah <nurfadilahmfauzi@gmail.com>
2026-06-22 14:56:35 +0700

    push

--- Pertemuan13/Praktikum/BE_Latihan/config/cors.go
+++ Pertemuan13/Praktikum/BE_Latihan/config/cors.go
@@ -2,6 +2,8 @@ package config
 
 var allowedOrigins = []string{
 	"http://localhost:5173",
+	"https://my-fe-three.vercel.app",
+	"https://be-latihan-production.up.railway.app",
 }
 
 func GetAllowedOrigins() []string {
